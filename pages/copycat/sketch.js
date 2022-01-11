3b06fb2a78c3376954d53a5293b3ebc263807b4f
agomez12 <andreaxing@gmail.com>
2022-01-10 22:46:26 -0800

    oops

--- pages/copycat/sketch.js
+++ pages/copycat/sketch.js
@@ -15,6 +15,9 @@ var letterBoxH = 55;
 var whatGuessAreWeOn = 0;
 var word = "TOAST"
 
+var interactX;
+var interactY;
+
 var copycat = function(a)
 {
     a.setup = function()
@@ -154,6 +157,8 @@ var copycat = function(a)
 
     a.mouseClicked = function()
     {
+        interactX = mouseX;
+        interactY = mouseY;
         for (var key of keys)
         {
             key.checkIfLetterClick();
