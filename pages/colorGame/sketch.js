d291875a59908dfe4b8c0792cf0094fb24ea11b0
gravysloth <axgomez@usc.edu>
2023-11-14 22:33:44 -0800

    reset button

--- pages/colorGame/sketch.js
+++ pages/colorGame/sketch.js
@@ -14,12 +14,17 @@ let threshold = 10
 let rgbRandom
 let rgb = ["red", "green", "blue"]
 
+let outputText = "select color"
+
+let resetButton
+
 function setup() {
   var canvas = createCanvas(600, 600)
   canvas.parent("sketch")
   frameRate(30)
   noStroke()
 
+  resetButton = new Button(createVector(width/2 - 50, 500), 100, 50, color(255), "NEXT")
   for (let i = 0; i < 3; i++) {
     let colorBase = [0, 0, 0]
     colorBase[i] = 255
@@ -28,14 +33,9 @@ function setup() {
       100,
       color(colorBase)))
   }
-
-  targetColor = getRandomColor()
   targetCircle = new ColorCircle(createVector(450, 150), 200, targetColor)
-  let targetColorArray = [red(targetColor), green(targetColor), blue(targetColor)]
-  rgbRandom = getRandomInt(0, 2)
-  targetColorArray[rgbRandom] -= threshold
-  changeColor = color(targetColorArray[0], targetColorArray[1], targetColorArray[2])
   changeCircle = new ColorCircle(createVector(150, 150), 200, changeColor)
+  resetGame()
 }
 
 function draw() {
@@ -45,6 +45,24 @@ function draw() {
   for (let i = 0; i < baseCircles.length; i++) {
     baseCircles[i].draw()
   }
+  resetButton.draw()
+
+  push()
+  fill(255)
+  textSize(16)
+  textAlign(CENTER)
+  text(outputText, width / 2, height - 150)
+  pop()
+}
+
+function resetGame() {
+  targetColor = getRandomColor()
+  let targetColorArray = [red(targetColor), green(targetColor), blue(targetColor)]
+  rgbRandom = getRandomInt(0, 2)
+  targetColorArray[rgbRandom] -= threshold
+  changeColor = color(targetColorArray[0], targetColorArray[1], targetColorArray[2])
+  targetCircle.color = targetColor
+  changeCircle.color = changeColor
 }
 
 function mouseClicked() {
@@ -52,15 +70,16 @@ function mouseClicked() {
   for (let i = 0; i < baseCircles.length; i++) {
     if (checkMouseWithinCircle(baseCircles[i])) {
       if (i == rgbRandom) {
-        console.log("YESSSS")
-        console.log("It was " + rgb[rgbRandom])
+        outputText = "YESSSS! It was " + rgb[rgbRandom]
       } else {
-        console.log("NOOO")
-        console.log("It's actually " + rgb[rgbRandom])
+        outputText = "NOOO. It's actually " + rgb[rgbRandom]
       }
     }
   }
-
+  if (checkMouseWithinButton(resetButton)) {
+    resetGame()
+    console.log("yes")
+  }
 }
 
 function checkMouseWithinCircle(ColorCircle) {
@@ -74,7 +93,15 @@ function checkMouseWithinCircle(ColorCircle) {
 function checkMouseWithinRect(position, width, height) {
   if ((mouseX > position.x && mouseX < position.x + width)
     && (mouseY > position.y && mouseY < position.y + height)) {
-    console.log("rect")
+    return true
+  } else {
+    return false
+  }
+}
+
+function checkMouseWithinButton(button) {
+  if ((mouseX > button.position.x && mouseX < button.position.x + button.width)
+    && (mouseY > button.position.y && mouseY < button.position.y + button.height)) {
     return true
   } else {
     return false
@@ -93,17 +120,22 @@ function getRandomInt(min, max) {
 }
 
 class Button {
-  constructor(position, width, height, color) {
+  constructor(position, width, height, color, buttonText) {
     this.position = position
     this.width = width
     this.height = height
     this.color = color
+    this.buttonText = buttonText
   }
 
   draw() {
     push()
     fill(this.color)
-    rect(this.position.x, this.position.y, this.width, this.height)
+    rect(this.position.x, this.position.y, this.width, this.height, 5)
+    fill(0)
+    textAlign(CENTER, CENTER)
+    textSize(24)
+    text(this.buttonText, this.position.x + this.width/2, this.position.y + this.height/2)
     pop()
   }
 }
